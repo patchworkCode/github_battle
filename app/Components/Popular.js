a86df8c42b54655d5e3ac8cb1f7ea59dda068d8f
Dhan-nun Ahmed <62321034+patchworkCode@users.noreply.github.com>
2021-12-07 21:07:14 +1300

    rudimentary caching

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -30,7 +30,7 @@ export default class Popular extends React.Component{
 
         this.state = {
             selectedLanguage: 'All',
-            repos : null,
+            repos : {},
             error : null
         }
 
@@ -44,24 +44,34 @@ export default class Popular extends React.Component{
         this.setState({
             selectedLanguage,
             error : null,
-            repos : null
         })
 
-        fetchPopularRepos(selectedLanguage)
-            .then((repos) => this.setState({
-                repos,
-                error :null
-            }))
-            .catch(() => {
-                console.warn('Error fetching the repositories: ', error)
+        if (!this.state.repos[selectedLanguage]) {
 
-                this.setState({
-                    error: 'There was an error fetching the repositories'
+            fetchPopularRepos(selectedLanguage)
+                .then((data) => {
+                    this.setState(({repos}) => ({
+                        repos: {
+                            ...repos,
+                            [selectedLanguage]: data //syntactic sugar for repos[SL] = data, breaks otherwise
+                        }
+                    }))
                 })
-            })
+                .catch(() => {
+                    console.warn('Error fetching the repositories: ', error)
+
+                    this.setState({
+                        error: 'There was an error fetching the repositories'
+                    })
+                })
+        }
+
+        
     }
     isLoading(){
-        return this.state.repos === null && this.state.error === null 
+        const {selectedLanguage, repos, error} = this.state
+
+        return !repos[selectedLanguage] && error === null
     }
     render(){
         const {selectedLanguage, repos, error} = this.state
@@ -76,7 +86,7 @@ export default class Popular extends React.Component{
 
                 {error && <p>{error}</p>}
 
-                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+                {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
             </React.Fragment>
         )
     }
